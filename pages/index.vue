72013e34626012c77f910f186babca2685d946c3
Ihsan MSTF <ihsanmchink@gmail.com>
2018-02-01 01:58:15 +0700

    Fix UX-2 Scrolling Input

--- pages/index.vue
+++ pages/index.vue
@@ -159,19 +159,17 @@ export default {
     },
     'AlgoliaQuery' () {
       let _self = this
-      if (_self.AlgoliaQuery.length > 1) {
-        if (_self.$store.state.isMobile) {
-          window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight) - 125))
-        } else {
-          window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight)))
-        }
+      if (_self.$store.state.isMobile) {
+        window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight) - 133))
+      } else {
+        window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight)))
       }
     },
     'ShowContent.active' (v) {
       let _self = this
       if (!v) {
         if (_self.$store.state.isMobile) {
-          window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight) - 125))
+          window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight) - 133))
         } else {
           window.scrollTo(0, parseInt(document.body.scrollHeight) - (parseInt(document.body.scrollHeight)))
         }

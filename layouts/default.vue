d5a03f935e41f7db23fe37051be01b6d59e8f106
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-02-01 23:34:09 +0700

    add lock to gitignore

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,12 +34,12 @@
           :replace="$route.name !== 'index'"
           exact
         >
-            <v-list-tile-action>
-              <v-icon v-html="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-            </v-list-tile-content>
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
